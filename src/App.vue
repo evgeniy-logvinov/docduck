76ea404a9f01561dfaa8fc117b43d5e59676f37b
Vlad Serikov <vladislav.serikov@grapecity.com>
2020-05-01 22:04:25 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app"><Header /></div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+export default {
+    name: 'app',
+    components: {
+        Header,
+    },
+};
+</script>
+
+<style lang="sass"></style>
